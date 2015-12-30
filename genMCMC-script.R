9b0cee84b425d48460826f4e2e15819febdfa765
nature-sky <vbnwerbnm@gmail.com>
2015-12-30 21:34:30 +0800

    Add MCMC model and use example code.

---
+++ genMCMC-script.R
@@ -0,0 +1,55 @@
+# Example for Jags-Ybinom-XnomSsubjCcat-MbinomBetaOmegaKappa.R 
+#------------------------------------------------------------------------------- 
+# Optional generic preliminaries:
+graphics.off() # This closes all of R's graphics windows.
+rm(list=ls())  # Careful! This clears all of R's memory!
+#------------------------------------------------------------------------------- 
+# Read the data 
+myData = read.csv("air-result.csv")
+#------------------------------------------------------------------------------- 
+# Load the relevant model into R's working memory:
+source("Jags-Ybinom-XnomSsubjCcat-MbinomBetaOmegaKappa.R")
+#------------------------------------------------------------------------------- 
+# Optional: Specify filename root and graphical format for saving output.
+# Otherwise specify as NULL or leave saveName and saveType arguments 
+# out of function calls.
+fileNameRoot = "air-result-POST-" 
+graphFileType = "pdf" 
+#------------------------------------------------------------------------------- 
+# Generate the MCMC chain:
+startTime = proc.time()
+mcmcCoda = genMCMC( data=myData , 
+                    # The column in our data
+                    zName="X00", NName="Dummy", sName="日期", cName="Season",
+                    numSavedSteps=500 , saveName=fileNameRoot ,
+                    thinSteps=20 )
+stopTime = proc.time()
+elapsedTime = stopTime - startTime
+show(elapsedTime)
+#------------------------------------------------------------------------------- 
+# Display diagnostics of chain, for specified parameters:
+parameterNames = varnames(mcmcCoda) # get all parameter names for reference
+for ( parName in c("omega[1]","omegaO","kappa[1]","kappaO","theta[1]") ) { 
+  diagMCMC( codaObject=mcmcCoda , parName=parName , 
+            saveName=fileNameRoot , saveType=graphFileType )
+}
+#------------------------------------------------------------------------------- 
+# Get summary statistics of chain:
+summaryInfo = smryMCMC( mcmcCoda , compVal=NULL , 
+                        #diffSVec=c(75,156, 159,844) , 
+                        diffCVec=c(1,2,3,4) , # Four Season
+                        compValDiff=0.0 , saveName=fileNameRoot )
+# Display posterior information:
+plotMCMC( mcmcCoda , data=myData , 
+          # The column in our data
+          zName="X00", NName="Dummy", sName="日期", cName="Season", 
+          compVal=NULL ,
+          diffCList=list( c("Spring","Summer") , # Compare Spring and Summer
+                          c("Autumn","Winter") ) , # Compare Autumn and Winter
+          diffSList=list( c("2014/01/04","2014/06/13") ), # Compare two dates
+                         # c("Mike Leake","Wandy Rodriguez") , 
+                          #c("Andrew McCutchen","Brett Jackson") , 
+                          #c("ShinSoo Choo","Ichiro Suzuki") ) , 
+          compValDiff=0.0, #ropeDiff = c(-0.05,0.05) ,
+          saveName=fileNameRoot , saveType=graphFileType )
+#------------------------------------------------------------------------------- 
