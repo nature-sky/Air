6437afef7d98b6f1f1a8a1ebd0eec369b0a6015a
Cameradog <kay821221@gmail.com>
2016-01-04 21:14:00 +0800

    update all r code

--- genMCMC-Station.R
+++ genMCMC-Station.R
@@ -5,7 +5,7 @@ graphics.off() # This closes all of R's graphics windows.
 rm(list=ls())  # Careful! This clears all of R's memory!
 #------------------------------------------------------------------------------- 
 # Read the data 
-myData = read.csv("output-season.csv")
+myData = read.csv("output-station.csv")
 #------------------------------------------------------------------------------- 
 # Load the relevant model into R's working memory:
 source("genMCMC.R")
@@ -13,16 +13,16 @@ source("genMCMC.R")
 # Optional: Specify filename root and graphical format for saving output.
 # Otherwise specify as NULL or leave saveName and saveType arguments 
 # out of function calls.
-fileNameRoot = "air-result-Season-" 
+fileNameRoot = "air-result-Station-" 
 graphFileType = "pdf" 
 #-------------------------------------------r------------------------------------ 
 # Generate the MCMC chain:
 startTime = proc.time()
 mcmcCoda = genMCMC( data=myData , 
                     # The column in our data
-                    zName="Day1", NName="Dummy", sName="DateStation", cName="Season",
+                    zName="Day1", NName="Dummy", sName="DateStation", cName="Station",
                     numSavedSteps=500 , saveName=fileNameRoot ,
-                    thinSteps=20)
+                    thinSteps=20 )
 stopTime = proc.time()
 elapsedTime = stopTime - startTime
 show(elapsedTime)
@@ -37,17 +37,22 @@ for ( parName in c("omega[1]","omegaO","kappa[1]","kappaO","theta[1]") ) {
 # Get summary statistics of chain:
 summaryInfo = smryMCMC( mcmcCoda , compVal=NULL , 
                         #diffSVec=c(75,156, 159,844) , 
-                        diffCVec=c(1,2,3,4) , # Four Season
+                        #diffCVec=c(1,2,3,4) , # Four Season
                         compValDiff=0.0 , saveName=fileNameRoot )
 # Display posterior information:
 plotMCMC( mcmcCoda , data=myData , 
           # The column in our data
-          zName="Day1", NName="Dummy", sName="Date", cName="Season", 
+          zName="Day1", NName="Dummy", sName="DateStation", cName="Station", 
           compVal=NULL ,
-          diffCList=list( c("Spring","Summer") ,
-                          c("Summer","Autumn") ,
-                          c("Autumn","Winter") ,# Compare Spring and Summer
-                          c("Winter","Spring") ) , # Compare Autumn and Winter
+          diffCList=list( c("Tamshui","Erlin") ,
+                          c("Erlin","Tainan") ,# Compare Spring and Summer
+                          c("Tamshui","Tainan") ,
+                          c("Taoyuan","Shalu") ,
+                          c("Shalu","Fengshan") ,
+                          c("Taoyuan","Fengshan") ,
+                          c("Sanchong","Changhua") ,
+                          c("Changhua","Pingtung") ,
+                          c("Sanchong","Pingtung") ) , # Compare Autumn and Winter
           #diffSList=list( c("2014/01/04","2014/06/13") ), # Compare two dates
           # c("Mike Leake","Wandy Rodriguez") , 
           #c("Andrew McCutchen","Brett Jackson") , 
